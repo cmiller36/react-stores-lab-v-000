cb45e9b21e41e37a09c025df81bf0ee093d8e514
cmiller36 <m.calliemiller@gmail.com>
2017-03-14 19:12:33 -0400

    Done.

--- components/App.js
+++ components/App.js
@@ -1,27 +1,44 @@
 const React = require('react');
+const counterStore = require('../stores/counterStore')
+const actions = require('../actions/index')
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      // Your implementation here.
+      counter: counterStore.getState()
     };
+    this.handleIncrement = this.handleIncrement.bind(this)
+    this.handleDecrement = this.handleDecrement.bind(this)
   }
   componentDidMount () {
-    // Your implementation here.
+    this.removeListener = counterStore.addListener(counter => {
+      this.setState({ counter })
+    });
+    this.setState(counterStore.getState())
   }
   componentWillUnmount () {
-    // Your implementation here.
+    this.removeListener();
+  }
+
+  handleIncrement (event) {
+    event.preventDefault()
+    actions.increment()
+  }
+
+  handleDecrement (event) {
+    event.preventDefault()
+    actions.decrement()
   }
   render () {
     return (
       <div className='app'>
-        <h1 className='counter'></h1>
+        <h1 className='counter'>{this.state.counter}</h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button className='decrement' onClick={this.handleDecrement}>
             -
           </button>
-          <button className='increment'>
+          <button className='increment' onClick={this.handleIncrement}>
             +
           </button>
         </div>
--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,6 +1,21 @@
-class CounterStore {
+const Store = require('./Store')
+
+class CounterStore extends Store{
   // Your implementation here.
   // Hint: extend the Store class!
+  constructor () {
+    super(0)
+  }
+
+  increment () {
+    const oldState = this.getState()
+    this.setState(oldState + 1)
+  }
+
+  decrement () {
+    const oldState = this.getState()
+    this.setState(oldState - 1)
+  }
 }
 
 module.exports = new CounterStore();
